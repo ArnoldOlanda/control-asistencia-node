663424099996f63f040be97dcb1685106c3d4ad3
ArnoldOlanda <olanda188@gmail.com>
2022-01-09 20:50:01 -0500

    Corregido error referente al puerto de escucha del servidor

--- src/config/server.js
+++ src/config/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"../views")); 
-app.set("port", process.env.port || 4000);
+app.set("port", process.env.PORT || 4000);
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
