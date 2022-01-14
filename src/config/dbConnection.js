40fe6e17a0bab25b0b85df11cc2b67ad3b207a0a
ArnoldOlanda <olanda188@gmail.com>
2022-01-13 20:29:12 -0500

    V1.0.2 Cambiada la base de datos de desarrollo a produccion

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -4,23 +4,23 @@
 const mysql = require("mysql2");
 
 //-------------------Produccion--------------------------------
-// module.exports = () => {
-//   return mysql.createPool({
-//     host:'us-cdbr-east-05.cleardb.net',
-//     user: 'b8f01177a02349',
-//     password:'e3634958',
-//     database: 'heroku_375a48be8c216d2'
-//   });
-// };
-
-//------------------Desarrollo----------------------------------
 module.exports = () => {
-  return mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password:'admin',
-    database: 'control_asistencia'
+  return mysql.createPool({
+    host:'us-cdbr-east-05.cleardb.net',
+    user: 'b8f01177a02349',
+    password:'e3634958',
+    database: 'heroku_375a48be8c216d2'
   });
 };
+
+//------------------Desarrollo----------------------------------
+// module.exports = () => {
+//   return mysql.createConnection({
+//     host:'localhost',
+//     user: 'root',
+//     password:'admin',
+//     database: 'control_asistencia'
+//   });
+// };
 
 //b8f01177a02349:e3634958@us-cdbr-east-05.cleardb.net/heroku_375a48be8c216d2
